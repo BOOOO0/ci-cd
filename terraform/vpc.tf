37fc5074b93f75cba6c3cf0cc6ff9dcac47e870d
Buyoung Chung <sunarol@naver.com>
2023-04-02 17:10:13 +0900

    feat: Add IGW, Route table

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -27,3 +27,36 @@ resource "aws_subnet" "web_subnet" {
 		Name = "web_server"
 	}
 }
+
+resource "aws_internet_gateway" "new_igw" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "new-igw"
+	}
+}
+
+resource "aws_route_table" "new_public_rtb" {
+	vpc_id = aws_vpc.main.id
+	
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.new_igw.id
+	}
+	
+	tags = {
+		Name = "new-public-rtb"
+	}
+}
+
+resource "aws_route_table_association" "jenkins_subnet_association" {
+	subnet_id = aws_subnet.jenkins_subnet.id
+	
+	route_table_id = aws_route_table.new_public_rtb.id
+}
+
+resource "aws_route_table_association" "web_subnet_association" {
+	subnet_id = aws_subnet.web_subnet.id
+
+	route_table_id = aws_route_table.new_public_rtb.id
+}
